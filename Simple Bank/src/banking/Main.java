eac067a55466cf89a7ce5db35c9226a674bcfcdc
Ilya <66162935+Rebel-Illich@users.noreply.github.com>
2020-11-17 21:54:40 +0200

    Added class Application

--- Simple Bank/src/banking/Main.java
+++ Simple Bank/src/banking/Main.java
@@ -22,7 +22,10 @@ public class Main {
 
      public static void main(String[] args) {
           log.info("Simple Banking System started.");
-          System.out.println("Hello World");
+          System.out.println("Hello World!!!");
+
+
+          new Application().run();
      }
 
 
