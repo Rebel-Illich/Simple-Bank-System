1b380f37e898499eb9a4d221d9e3ff3a625942a8
Ilya <66162935+Rebel-Illich@users.noreply.github.com>
2020-11-17 23:45:13 +0200

    Update AccountsRepository.java

--- Simple Bank/src/banking/AccountsRepository.java
+++ Simple Bank/src/banking/AccountsRepository.java
@@ -1,5 +1,11 @@
 package banking;
 
 public interface AccountsRepository {
-    
+    void addAccount(Account account);
+
+    default Account createAccount(){
+        Account account = new Account();
+        addAccount(account);
+        return account;
+    }
 }
