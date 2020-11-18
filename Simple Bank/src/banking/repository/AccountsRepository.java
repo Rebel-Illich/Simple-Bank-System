cfb3eb8cef577b04bb7412432925a719bc498033
Ilya <66162935+Rebel-Illich@users.noreply.github.com>
2020-11-18 10:50:00 +0200

    Added method getAccount() in interface AccountsRepository

--- Simple Bank/src/banking/repository/AccountsRepository.java
+++ Simple Bank/src/banking/repository/AccountsRepository.java
@@ -1,6 +1,7 @@
 package banking.repository;
 
 import banking.Account;
+import java.util.Optional;
 
 public interface AccountsRepository {
     void addAccount(Account account);
@@ -10,4 +11,6 @@ public interface AccountsRepository {
         addAccount(account);
         return account;
     }
+
+    Optional<Account> getAccount(final String creditCardNumber, final String pinNumber);
 }
