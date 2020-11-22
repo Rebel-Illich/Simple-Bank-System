851fbd84bf5f3d79df3cdd88f07b6fc586ac81ba
Ilya <66162935+Rebel-Illich@users.noreply.github.com>
2020-11-22 23:34:56 +0200

    Created class SQLiteDatabase

--- Simple Bank/src/banking/repository/SQLiteDatabase.java
+++ Simple Bank/src/banking/repository/SQLiteDatabase.java
@@ -1,6 +1,27 @@
 package banking.repository;
+import banking.Account;
+
+import java.util.Optional;
 
 public class SQLiteDatabase implements AccountsRepository {
-    public SQLiteDatabase(String arg) {
+    private final String databaseName;
+    public SQLiteDatabase(String databaseName) {
+        this.databaseName = databaseName;
     }
+
+    @Override
+    public void addAccount(Account account) {
+
+    }
+
+    @Override
+    public Optional<Account> getAccount(String creditCardNumber, String pinNumber) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Account createAccount() {
+        return null;
+    }
+    
 }
